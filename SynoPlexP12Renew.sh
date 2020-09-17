92f1b28c32e83895102f7b98a0c900d6a09931e0
Felix <25091279+x-Felix@users.noreply.github.com>
2020-09-17 13:02:53 +1000

    Skip renew check

--- SynoPlexP12Renew.sh
+++ SynoPlexP12Renew.sh
@@ -10,40 +10,40 @@ p12cert_password=xxxxxxxxxxxxxxxxxxxx
 # Synology's Default Let's encrypt folder
 letsencrypt_cert_folder=/usr/syno/etc/certificate/_archive/RANDOM FOLDER NAME
 # renew timestamp
-renew_timestamp=renew_plex_timestamp
+#renew_timestamp=renew_plex_timestamp
 
 
 # DO NOT CHANGE BELOW UNLESS YOU'RE A WIZARD
 
-generate_p12=false
-current_date=`date +"%s"`
-current_certificate_date=`openssl x509 -enddate -noout -in $letsencrypt_cert_folder/cert.pem | cut -d'=' -f2`
-current_certificate_timestamp=`date -d "$current_certificate_date" +"%s"`
+generate_p12=true
+#current_date=`date +"%s"`
+#current_certificate_date=`openssl x509 -enddate -noout -in $letsencrypt_cert_folder/cert.pem | cut -d'=' -f2`
+#current_certificate_timestamp=`date -d "$current_certificate_date" +"%s"`
 
 # check if the renew_timestamp file exists
-if [ ! -f $script_folder/$renew_timestamp ]; then
-  echo "Generate timestamp for the current renew date... "
-  echo $current_certificate_timestamp > $script_folder/$renew_timestamp
-  chmod +rw $script_folder/$renew_timestamp
-  chown admin:users $script_folder/$renew_timestamp
+#if [ ! -f $script_folder/$renew_timestamp ]; then
+#  echo "Generate timestamp for the current renew date... "
+#  echo $current_certificate_timestamp > $script_folder/$renew_timestamp
+#  chmod +rw $script_folder/$renew_timestamp
+#  chown admin:users $script_folder/$renew_timestamp
 
 # generate the first p12 file
-  generate_p12=true
-else
-  renew_date=`cat $script_folder/$renew_timestamp`
+#  generate_p12=true
+#else
+#  renew_date=`cat $script_folder/$renew_timestamp`
   # check if it is necessary to renew the certificate or not
-  if expr "$current_date" ">" "$renew_date" > /dev/null; then
+#  if expr "$current_date" ">" "$renew_date" > /dev/null; then
     # generate a new p12 file
-      echo "Renewing certificate..."
-      generate_p12=true
+#      echo "Renewing certificate..."
+#      generate_p12=true
 
    # update timestamp in the file
-  echo $current_certificate_timestamp > $script_folder/$renew_timestamp
- else
- echo "It is not necessary to renew the certificate, abort."
-   exit 0
-                  fi
-  fi
+#  echo $current_certificate_timestamp > $script_folder/$renew_timestamp
+# else
+# echo "It is not necessary to renew the certificate, abort."
+#   exit 0
+#                  fi
+#  fi
 
 # generate a new certificate file if necessary, and restart Plex
   if expr "$generate_p12" "=" "true" > /dev/null; then
